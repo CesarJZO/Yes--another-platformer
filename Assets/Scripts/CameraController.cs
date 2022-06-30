3d405ab861b6e537f844230149c185c4379fac79
CesarJZO <cesarjzo@outlook.com>
2022-06-30 18:33:05 -0500

    Add CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        var desiredPosition = target.position + offset;
+        desiredPosition.y = 0;
+        var smooth = Vector3.Lerp(transform.position, desiredPosition, speed);
+        transform.position = smooth;
+    }
+}
