01919c103338ff0cb27d8b8b5a1ff34882f3b2ba
CesarJZO <cesarjzo@outlook.com>
2022-07-02 00:28:27 -0500

    Add animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,62 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    private float _horizontalInput;
     public bool isAlive = true;
-    private PlayerMovement _movement;
 
+    private PlayerMovement _movement;
     private Rigidbody2D _rigidbody;
     public GameManager gameManager;
     
     private InputAction _move;
+
+    #region Animation
+
+    private Animator _animator;
+    private int _moveAnimId;
+    private int _jumpAnimId;
+    private int _groundAnimId;
+    private int _isAliveAnimId;
+    private int _dieAnimId;
+    private int _vertAnimId;
+
+    #endregion
 
     private void Awake()
     {
         _movement = GetComponent<PlayerMovement>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _move = GetComponent<PlayerInput>().actions[ActionName.Move.ToString()];
+        
+        _animator = GetComponent<Animator>();
+        _moveAnimId = Animator.StringToHash("Move");
+        _jumpAnimId = Animator.StringToHash("Jump");
+        _groundAnimId = Animator.StringToHash("Grounded");
+        _isAliveAnimId = Animator.StringToHash("Is Alive");
+        _dieAnimId = Animator.StringToHash("Die");
+        _vertAnimId = Animator.StringToHash("Vertical Velocity");
     }
 
     private void FixedUpdate()
     {
         if (!isAlive) return;
-        _movement.Move(_move.ReadValue<float>() * speed);
+        _horizontalInput = _move.ReadValue<float>(); 
+        _movement.Move(_horizontalInput * speed);
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetBool(_moveAnimId, Mathf.Abs(_horizontalInput) > 0.01);
+        _animator.SetFloat(_vertAnimId, _rigidbody.velocity.y);
+        _animator.SetBool(_groundAnimId, _movement.grounded);
+        _animator.SetBool(_isAliveAnimId, isAlive);
     }
 
     public void OnJump()
     {
         if (!isAlive) return;
         _movement.Jump();
+        _animator.SetTrigger(_jumpAnimId);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -67,6 +98,7 @@ public class PlayerController : MonoBehaviour
     private void Die()
     {
         isAlive = false;
+        _animator.SetTrigger(_dieAnimId);
         _rigidbody.velocity = Vector2.zero;
     }
 }
